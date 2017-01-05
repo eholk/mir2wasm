2fdc2fdb2f1b6558fab0f75445babf0f7d83eb4b
Eric Holk <eholk@google.com>
2017-01-04 18:52:05 -0800

    All tests pass on linux

--- src/trans.rs
+++ src/trans.rs
@@ -777,7 +777,7 @@ impl<'v, 'tcx: 'v, 'module: 'v> BinaryenFnCtxt<'v, 'tcx, 'module> {
                 let wasm_start = self.generate_runtime_start(&entry_fn_name);
                 debug!("emitting wasm Start fn into entry_fn {:?}",
                        self.tcx.item_path_str(self.did));
-                //BinaryenSetStart(self.func.module.module, wasm_start);
+                BinaryenSetStart(self.func.module.module, wasm_start);
             }
         }
 
